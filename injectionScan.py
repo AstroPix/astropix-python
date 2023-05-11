d41fa22486f077ece49c4da7c6c0c4c2e987f45c
gs66c235 <gs66c235@ndc.nasa.gov>
2023-05-11 09:57:44 -0400

    update injectionScan script to work more intuitively, also can be used with v3 or v2

--- injectionScan.py
+++ injectionScan.py
@@ -4,8 +4,6 @@ Script to loop through injection voltage values enabling a single pixel. Test al
 Author: Amanda Steinhebel
 """
 
-#from msilib.schema import File
-#from http.client import SWITCHING_PROTOCOLS
 from astropix import astropixRun
 import modules.hitplotter as hitplotter
 import os
@@ -25,8 +23,6 @@ if os.path.exists(logdir) == False:
     os.mkdir(logdir)
 logname = "./runlogs/AstropixRunlog_" + time.strftime("%Y%m%d-%H%M%S") + ".log"
 
-
-
 # This is the dataframe which is written to the csv if the decoding fails
 decode_fail_frame = pd.DataFrame({
                 'readout': np.nan,
@@ -43,50 +39,41 @@ decode_fail_frame = pd.DataFrame({
                 }, index=[0]
 )
 
-  
-
-#Initialize
-def main(args,fpgaCon:bool=True, fpgaDiscon:bool=True):
+#Init 
+def main(args, injv, fpgaCon:bool=True, fpgaDiscon:bool=True):
 
     # Ensures output directory exists
     if os.path.exists(args.outdir) == False:
         os.mkdir(args.outdir)
-        
+
     if fpgaCon:
         # Prepare everything, create the object
         global astro 
         logger.info('Initiate FPGA connection')
-        astro = astropixRun(inject=args.inject) #initialize without enabling injections
+        astro = astropixRun(chipversion=args.chipVer, inject=args.inject) #initialize with always enabling injections (args.inject is always true)
 
-    astro.init_voltages(vthreshold=args.threshold) #no updates in YAML
+    astro.init_voltages(vthreshold=args.threshold) 
 
     #Define YAML path variables
     pathdelim=os.path.sep #determine if Mac or Windows separators in path name
-    ymlpath="config"+pathdelim+args.yaml+".yml"
 
-    #Initiate asic with pixel mask as defined in yaml and analog pixel in row0 defined with input argument -a
-    astro.asic_init(yaml=ymlpath, analog_col = args.analog)
+    #Initiate asic with pixel mask as defined in yaml 
+    astro.asic_init(yaml=config, analog_col=args.inject[1])
 
-    #If injection, ensure injection pixel is enabled and initialize
-    if args.inject is not None:
-        astro.enable_pixel(args.inject[1],args.inject[0])    
-        astro.init_injection(inj_voltage=args.vinj)
+    #enable injection
+    astro.enable_pixel(args.inject[1],args.inject[0])    
+    astro.init_injection(inj_voltage=injv, onchip=onchipBool)
 
-    #Enable final configuration
     astro.enable_spi() 
     logger.info("Chip configured")
     astro.dump_fpga()
+    astro.start_injection()
 
-    if args.inject is not None:
-        astro.start_injection()
-
-
-    max_errors = args.errormax
     i = 0
-    errors = 0 # Sets the threshold 
     if args.maxtime is not None: 
         end_time=time.time()+(args.maxtime*60.)
-    fname="" if not args.name else newname+"_"
+    strPix = "r"+str(args.inject[0])+"c"+str(args.inject[1])+"_"+str(injv/1000.)+"VInj_"
+    fname=strPix if not args.name else args.name+strPix+"_"
 
     # Prepares the file paths 
     if args.saveascsv: # Here for csv
@@ -104,102 +91,52 @@ def main(args,fpgaCon:bool=True, fpgaDiscon:bool=True):
                 'tot_us',
                 'hittime'
         ])
-
     # Save final configuration to output file    
-    ymlpathout=args.outdir +pathdelim+args.yaml+"_"+time.strftime("%Y%m%d-%H%M%S")+".yml"
+    ymlpathout=args.outdir+pathdelim+config+"_"+fname+time.strftime("%Y%m%d-%H%M%S")+".yml"
     astro.write_conf_to_yaml(ymlpathout)
-    # Prepare text files/logs
-    bitpath = args.outdir + '/' + fname + time.strftime("%Y%m%d-%H%M%S") + '.log'
-    # textfiles are always saved so we open it up 
+    # And here for the text files/logs
+    bitpath = args.outdir + pathdelim + fname + time.strftime("%Y%m%d-%H%M%S") + '.log'
     bitfile = open(bitpath,'w')
     # Writes all the config information to the file
     bitfile.write(astro.get_log_header())
     bitfile.write(str(args))
     bitfile.write("\n")
 
-    # Enables the hitplotter and uses logic on whether or not to save the images
-    if args.showhits: plotter = hitplotter.HitPlotter(35, outdir=(args.outdir if args.plotsave else None))
-
-    try: # By enclosing the main loop in try/except we are able to capture keyboard interupts cleanly
-        
-        while errors <= max_errors: # Loop continues 
-
-            # This might be possible to do in the loop declaration, but its a lot easier to simply add in this logic
-            if args.maxruns is not None:
-                if i >= args.maxruns: break
+    try: # By enclosing the main loop in try/except we are able to capture keyboard interupts cleanly    
+        while (True): # Loop continues 
+            # Break conditions
             if args.maxtime is not None:
                 if time.time() >= end_time: break
             
-            
             if astro.hits_present(): # Checks if hits are present
-                # We aren't using timeit, just measuring the diffrence in ns
-                if args.timeit: start = time.time_ns()
     
                 time.sleep(.001) # this is probably not needed, will ask Nicolas
 
                 readout = astro.get_readout(3) # Gets the bytearray from the chip
 
-                if args.timeit:
-                    print(f"Readout took {(time.time_ns()-start)*10**-9}s")
-
                 # Writes the hex version to hits
                 bitfile.write(f"{i}\t{str(binascii.hexlify(readout))}\n")
                 print(binascii.hexlify(readout))
-
-                # Added fault tolerance for decoding, the limits of which are set through arguments
-                try:
-                    hits = astro.decode_readout(readout, i, printer = True)
-
-                except IndexError:
-                    errors += 1
-                    logger.warning(f"Decoding failed. Failure {errors} of {max_errors} on readout {i}")
-                    # We write out the failed decode dataframe
-                    hits = decode_fail_frame
-                    hits.readout = i
-                    hits.hittime = time.time()
-
-                    # This loggs the end of it all 
-                    if errors > max_errors:
-                        logger.warning(f"Decoding failed {errors} times on an index error. Terminating Progam...")
-                finally:
-                    i += 1
-
-                    # If we are saving a csv this will write it out. 
-                    if args.saveascsv:
-                        csvframe = pd.concat([csvframe, hits])
-
-                    # This handels the hitplotting. Code by Henrike and Amanda
-                    if args.showhits:
-                        # This ensures we aren't plotting NaN values. I don't know if this would break or not but better 
-                        # safe than sorry
-                        if pd.isnull(hits.tot_msb.loc(0)):
-                            pass
-                        elif len(hits)>0:#safeguard against bad readouts without recorded decodable hits
-                            #Isolate row and column information from array returned from decoder
-                            rows = hits.location[~hits.isCol]
-                            columns = hits.location[hits.isCol]
-                            plotter.plot_event( rows, columns, i)
-
-                    # If we are logging runtime, this does it!
-                    if args.timeit:
-                        print(f"Read and decode took {(time.time_ns()-start)*10**-9}s")
+                hits = astro.decode_readout(readout, i, printer = True)
+                i += 1
+                # If we are saving a csv this will write it out. 
+                if args.saveascsv:
+                    csvframe = pd.concat([csvframe, hits])
 
             # If no hits are present this waits for some to accumulate
             else: time.sleep(.001)
-
-
     # Ends program cleanly when a keyboard interupt is sent.
     except KeyboardInterrupt:
         logger.info("Keyboard interupt. Program halt!")
     # Catches other exceptions
     except Exception as e:
         logger.exception(f"Encountered Unexpected Exception! \n{e}")
-    finally:
+    finally:  
+        astro.stop_injection()
         if args.saveascsv: 
             csvframe.index.name = "dec_order"
             csvframe.to_csv(csvpath) 
-        if args.inject is not None: astro.stop_injection()   
-        bitfile.close() # Close open file        
+        bitfile.close() # Close open file       
         if fpgaDiscon:
             astro.close_connection() # Closes SPI
             logger.info('FPGA Connection ended')
@@ -217,70 +154,34 @@ if __name__ == "__main__":
 
     parser.add_argument('-o', '--outdir', default='.', required=False,
                     help='Output Directory for all datafiles')
-
-    parser.add_argument('-y', '--yaml', action='store', required=False, type=str, default = 'testconfig',
-                    help = 'filepath (in config/ directory) .yml file containing chip configuration. Default: config/testconfig.yml (All pixels off)')
-
-    parser.add_argument('-s', '--showhits', action='store_true',
-                    default=False, required=False,
-                    help='Display hits in real time during data taking')
     
-    parser.add_argument('-p', '--plotsave', action='store_true', default=False, required=False,
-                    help='Save plots as image files. If set, will be saved in  same dir as data. Default: FALSE')
-    
+    parser.add_argument('-V', '--chipVer', default=2, required=False, type=int,
+                    help='Chip version - provide an int')
+
     parser.add_argument('-c', '--saveascsv', action='store_true', 
                     default=False, required=False, 
                     help='save output files as CSV. If False, save as txt. Default: FALSE')
     
-    parser.add_argument('-i', '--inject', action='store', default=None, type=int, nargs=2,
-                    help =  'Turn on injection in the given row and column. Default: No injection')
-
-    parser.add_argument('-v','--vinj', action='store', default = None, type=float,
-                    help = 'Specify injection voltage (in mV). DEFAULT 300 mV')
-
-    parser.add_argument('-a', '--analog', action='store', required=False, type=int, default = 0,
-                    help = 'Turn on analog output in the given column. Default: Column 0.')
-
     parser.add_argument('-t', '--threshold', type = float, action='store', default=None,
-                    help = 'Threshold voltage for digital ToT (in mV). DEFAULT 100mV')
-    
-    parser.add_argument('-E', '--errormax', action='store', type=int, default='100', 
-                    help='Maximum index errors allowed during decoding. DEFAULT 100')
-
-    parser.add_argument('-r', '--maxruns', type=int, action='store', default=None,
-                    help = 'Maximum number of readouts')
+                    help = 'Threshold voltage for digital ToT (in mV). DEFAULT 80mV')
 
     parser.add_argument('-M', '--maxtime', type=float, action='store', default=None,
-                    help = 'Maximum run time (in minutes)')
+                    help = 'Maximum run time (in minutes) at each point')
 
-    parser.add_argument('--timeit', action="store_true", default=False,
-                    help='Prints runtime from seeing a hit to finishing the decode to terminal')
+    parser.add_argument('-i', '--inject', action='store', default=[0,0], type=int, nargs=2,
+                    help =  'Scan through injections in the given row and column. Default: 0 0')
+    
+    parser.add_argument('-I', '--injectRange', action='store', default=[100,1000], type=int, nargs=2,
+                    help =  'Range of injection voltages to scan through in mV. Default: 100 1000')
+    
+    parser.add_argument('-s', '--injectStep', action='store', default=100, type=float,
+                    help =  'Step used for scanning through injections in mV. Default: 100')
 
-    parser.add_argument('-L', '--loglevel', type=str, choices = ['D', 'I', 'E', 'W', 'C'], action="store", default='I',
-                    help='Set loglevel used. Options: D - debug, I - info, E - error, W - warning, C - critical. DEFAULT: D')
-    """
-    parser.add_argument('--ludicrous-speed', type=bool, action='store_true', default=False,
-                    help="Fastest possible data collection. No decode, no output, no file.\
-                         Saves bitstreams in memory until keyboard interupt or other error and then writes them to file.\
-                             Use is not generally recommended")
-    """
     parser.add_argument
     args = parser.parse_args()
-
-    # Sets the loglevel
-    ll = args.loglevel
-    if ll == 'D':
-        loglevel = logging.DEBUG
-    elif ll == 'I':
-        loglevel = logging.INFO
-    elif ll == 'E':
-        loglevel = logging.ERROR
-    elif ll == 'W':
-        loglevel = logging.WARNING
-    elif ll == 'C':
-        loglevel = logging.CRITICAL
     
-    # Logging 
+    # Logging
+    loglevel = logging.INFO
     formatter = logging.Formatter('%(asctime)s:%(msecs)d.%(name)s.%(levelname)s:%(message)s')
     fh = logging.FileHandler(logname)
     fh.setFormatter(formatter)
@@ -293,8 +194,21 @@ if __name__ == "__main__":
 
     logger = logging.getLogger(__name__)
 
-    injs = [100, 200, 300, 400, 500, 600, 700, 800, 900]
-    for i,inj in enumerate(injs):
-        newname = args.name + "_" + str(inj) + "mVinj"
-        args.vinj = inj
-        main(args)
+    #If using v2, use injection created by injection card
+    #If using v3, use injection created with integrated DACs on chip
+    onchipBool = True if args.chipVer > 2 else False
+
+    #If using v2, use config_none
+    #If using v3, use config_v3_none
+    config = 'config_v3_none' if args.chipVer > 2 else 'config_none'
+
+    injs = [args.injectRange[0]+(args.injectStep*x) for x in range(int((args.injectRange[1]-args.injectRange[0])/args.injectStep) + 1)]
+    for i in injs:
+        #loop through injection array with single pixel enabled, analog automatically enabled in whatever column is being injected into
+        if i==args.injectRange[0]:#first injection - connect to FPGA but leave open
+            main(args, float(i), fpgaDiscon=False)
+        elif i==args.injectRange[1]: #final injection - disconnect from FPGA upon completion
+            main(args, float(i), fpgaCon=False)
+        else: #for bulk of pixels, FPGA is already open. Do not reconnect and do not disconnect when completed, leave it open for the next injection
+            main(args, float(i), fpgaCon=False, fpgaDiscon=False)
+            
