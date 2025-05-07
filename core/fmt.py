9c05b5d7cef7df846991fb00ed59af6d53caa28a
Luca Baldini <luca.baldini@pi.infn.it>
2025-05-07 15:35:14 +0200

    Refactoring.

--- core/fmt.py
+++ core/fmt.py
@@ -80,6 +80,24 @@ class AbstractAstroPixHit(ABC):
     fields are arbitrary subsets of a multi-byte word, it seemed more natural to
     describe the hit as a sequence of fields, each one with its own length in bits.
 
+    Note this is an abstract class that cannot be instantiated. Concrete subclasses
+    must by contract:
+
+    * define the _LAYOUT class member, a dictionary mapping the names of the
+      fields in the underlying binary data structure to the corresponding width
+      in bits;
+    * define the _ATTRIBUTES class member, which typically contains all the keys
+      in the _LAYOUT dictionary, and, possibly, additional strings for class member
+      defined in the constructor---this is used to control the string formatting
+      and the file I/O;
+    * explicitely calculate the size (in bytes) of the hit structure. This can
+      be done by summing up the widths of the fields in _LAYOUT, and the staticmethod
+      ``_calculate_size()`` does just that. (Note this could be automatically done
+      at runtime, but since for simple, fixed-width hit strutures this is the same
+      for all the instances, it seemed more natural to accept the nuisance of doing
+      that in the class definition, avoiding the overhead that would be implied by
+      doing that over and over again for each object.)
+
     Arguments
     ---------
     data : bytearray
@@ -87,7 +105,7 @@ class AbstractAstroPixHit(ABC):
     """
 
     _LAYOUT = None
-    _FIELD_NAMES = None
+    _ATTRIBUTES = None
     SIZE = None
 
     def __init__(self, data: bytearray) -> None:
@@ -105,8 +123,12 @@ class AbstractAstroPixHit(ABC):
             pos += width
 
     @staticmethod
-    def calculate_size(layout: dict[str, int]) -> int:
-        """Calculate the size of a hit in bytes.
+    def _calculate_size(layout: dict[str, int]) -> int:
+        """Calculate the size of a concrete hit data structure in bytes.
+
+        This is achieved by summing up all the values in the _LAYOUT dictionary,
+        and does not include the size of the hit header and trailer within the
+        readout.
 
         Arguments
         ---------
@@ -114,9 +136,11 @@ class AbstractAstroPixHit(ABC):
             The layout of the hit, as a dictionary of field names and their
             respective widths in bits.
         """
-        size, reminder = divmod(sum(layout.values()), 8)
-        print(size, reminder)
-        return (sum(layout.values()) + 7) // 8
+        num_bits = sum(layout.values())
+        size, reminder = divmod(num_bits, 8)
+        if reminder != 0:
+            raise RuntimeError(f'Invalid layout {layout}: size in bit ({num_bits}) is not a multiple of 8')
+        return size
 
     @staticmethod
     def gray_to_decimal(gray: int) -> int:
@@ -190,19 +214,19 @@ class AbstractAstroPixHit(ABC):
         return f'{separator.join(vals)}\n'
 
     @classmethod
-    def text_header(cls, fields=None, separator=',') -> str:
+    def text_header(cls, attrs=None, separator=',') -> str:
         """Return a proper header for a text file representing a list of hits.
         """
-        if fields is None:
-            fields = cls._FIELD_NAMES
-        return ','.join(fields)
+        if attrs is None:
+            attrs = cls._ATTRIBUTES
+        return ','.join(attrs)
 
-    def to_csv(self, fields=None) -> str:
+    def to_csv(self, attrs=None) -> str:
         """Return the hit representation in csv format.
         """
-        if fields is None:
-            fields = self._FIELD_NAMES
-        return self._text(fields, fmts=None, separator=',')
+        if attrs is None:
+            attrs = self._ATTRIBUTES
+        return self._text(attrs, fmts=None, separator=',')
 
     def __eq__(self, other: 'AbstractAstroPixHit') -> bool:
         """Comparison operator---this is handy in the unit tests.
@@ -212,7 +236,7 @@ class AbstractAstroPixHit(ABC):
     def __str__(self) -> str:
         """String formatting.
         """
-        return self._repr(self._FIELD_NAMES)
+        return self._repr(self._ATTRIBUTES)
 
 
 class AstroPix3Hit(AbstractAstroPixHit):
@@ -235,8 +259,8 @@ class AstroPix3Hit(AbstractAstroPixHit):
         'tot_msb': 4,
         'tot_lsb': 8
     }
-    _FIELD_NAMES = tuple(_LAYOUT.keys()) + ('tot', 'tot_us')
-    SIZE = 5
+    _ATTRIBUTES = tuple(_LAYOUT.keys()) + ('tot', 'tot_us')
+    SIZE = AbstractAstroPixHit._calculate_size(_LAYOUT)
     CLOCK_CYCLES_PER_US = 200.
 
     def __init__(self, data: bytearray) -> None:
@@ -267,8 +291,8 @@ class AstroPix4Hit(AbstractAstroPixHit):
         'ts_fine2': 3,
         'ts_tdc2': 5
     }
-    _FIELD_NAMES = tuple(_LAYOUT.keys()) + ('ts_dec1', 'ts_dec2', 'tot_us', 'trigger_id', 'timestamp')
-    SIZE = 8
+    _ATTRIBUTES = tuple(_LAYOUT.keys()) + ('ts_dec1', 'ts_dec2', 'tot_us', 'trigger_id', 'timestamp')
+    SIZE = AbstractAstroPixHit._calculate_size(_LAYOUT)
     CLOCK_CYCLES_PER_US = 20.
     CLOCK_ROLLOVER = 2**17
 
@@ -317,15 +341,8 @@ class AbstractAstroPixReadout(ABC):
     to a binary file.
 
     A full readout comes in the form of a fixed-length bytearray object that is
-    padded at the end with a fixed byte (0xff).
-
-    What remains when the padding bytes have been removed should be a sequence of
-    frames of the form
-
-    bcbc ... hit data ... bcbcbcbcbcbc
-
-    where the hit data are 8-byte long and encapsulate all the information within
-    a single hit.
+    padded at the end with a padding byte (0xff). The hit data are surrounded by a
+    (generally variable) number of idle bytes (0xbc).
 
     Arguments
     ---------
@@ -342,7 +359,9 @@ class AbstractAstroPixReadout(ABC):
 
     # The padding byte used to pad the readout.
     _PADDING_BYTE = bytes.fromhex('ff')
-    # The readout header---this is prepended to the buffer read from the NEXYS board
+    # The idle byte,
+    _IDLE_BYTE = bytes.fromhex('bc')
+    # The readout header, which is prepended to the buffer read from the NEXYS board
     # before the thing gets written to disk.
     _HEADER = bytes.fromhex('fedcba')
     _HEADER_SIZE = len(_HEADER)
@@ -386,6 +405,16 @@ class AbstractAstroPixReadout(ABC):
         ---------
         input_file : BinaryIO
             A file object opened in "rb" mode.
+        """
+
+    @abstractmethod
+    def decode(self, reverse: bool = True) -> list[AbstractAstroPixHit]:
+        """Decode the underlying data and turn them into a list of hits.
+
+        Arguments
+        ---------
+        reverse : bool (default True)
+            If True, the bit order within each byte is reversed.
         """
 
     @staticmethod
@@ -483,6 +512,23 @@ class AstroPix4Readout(AbstractAstroPixReadout):
 
     def decode(self, reverse: bool = True) -> list[AstroPix4Hit]:
         """Decode the underlying data and turn them into a list of hits.
+
+        Here is some important details about the underlying generation of idle bytes,
+        verbatim from a comment by Nicolas to the github pull request
+        https://github.com/AstroPix/astropix-python/pull/22
+
+        Regarding the number of IDLE bytes ("BC"), you typically see two when you start
+        reading from the chip. This happens because the chip's SPI interface is clocked
+        by the SPI master in the DAQ. The data isn't immediately available because it
+        has to be transferred from an asynchronous FIFO to a FSM and then to the SPI
+        arbiter which needs few clock cycles. While the chip is preparing the data, it
+        sends out two IDLE bytes. After these 16 spi clock cycles, data is ready and
+        the chip can start transmitting. As the data is being read, the chip uses the
+        clock cycles to fetch new data, allowing multiple data words to be transmitted
+        without IDLE bytes in between or just one. The "line break" shown by @grant-sommer
+        is due to the fact that the firmware reads a certain number of bytes which is
+        not synchronized to beginning or ending of dataframes coming from the chip,
+        so it might just stop reading in the middle of a frame.
 
         Arguments
         ---------
